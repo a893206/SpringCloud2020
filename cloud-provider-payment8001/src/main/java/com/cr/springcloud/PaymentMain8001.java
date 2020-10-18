2f9700adc7d0b3d14820a75464e2564ad0fbf665
CR <931009686@qq.com>
2020-10-18 19:57:28 +0800

    支付微服务8001入驻Eureka Server

--- cloud-provider-payment8001/src/main/java/com/cr/springcloud/PaymentMain8001.java
+++ cloud-provider-payment8001/src/main/java/com/cr/springcloud/PaymentMain8001.java
@@ -2,12 +2,14 @@ package com.cr.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author cr
  * @date 2020-10-17 16:23
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class PaymentMain8001 {
     public static void main(String[] args) {
         SpringApplication.run(PaymentMain8001.class, args);
