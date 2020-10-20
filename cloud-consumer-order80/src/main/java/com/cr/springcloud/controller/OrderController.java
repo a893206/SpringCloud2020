54b98033d9409d43390ad7dde74a7dc9a0d346ae
CR <931009686@qq.com>
2020-10-20 11:18:18 +0800

    Ribbon的负载均衡和Rest调用

--- cloud-consumer-order80/src/main/java/com/cr/springcloud/controller/OrderController.java
+++ cloud-consumer-order80/src/main/java/com/cr/springcloud/controller/OrderController.java
@@ -4,6 +4,7 @@ import com.cr.springcloud.entities.CommonResult;
 import com.cr.springcloud.entities.Payment;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
@@ -26,8 +27,30 @@ public class OrderController {
         return restTemplate.postForObject(PAYMENT_URL, payment, CommonResult.class);
     }
 
+    @PostMapping("/postForEntity")
+    public CommonResult<Integer> create1(@RequestBody Payment payment) {
+        ResponseEntity<CommonResult> entity = restTemplate.postForEntity(PAYMENT_URL, payment, CommonResult.class);
+
+        if (entity.getStatusCode().is2xxSuccessful()) {
+            log.info(entity.getStatusCode() + "\t" + entity.getHeaders());
+            return entity.getBody();
+        }
+        return new CommonResult<>(444, "操作失败");
+    }
+
     @GetMapping("/{id}")
     public CommonResult<Payment> getPaymentById(@PathVariable Long id) {
         return restTemplate.getForObject(PAYMENT_URL + "/" + id, CommonResult.class);
+    }
+
+    @GetMapping("/getForEntity/{id}")
+    public CommonResult<Payment> getPaymentById1(@PathVariable Long id) {
+        ResponseEntity<CommonResult> entity = restTemplate.getForEntity(PAYMENT_URL + "/" + id, CommonResult.class);
+
+        if (entity.getStatusCode().is2xxSuccessful()) {
+            log.info(entity.getStatusCode() + "\t" + entity.getHeaders());
+            return entity.getBody();
+        }
+        return new CommonResult<>(444, "操作失败");
     }
 }
