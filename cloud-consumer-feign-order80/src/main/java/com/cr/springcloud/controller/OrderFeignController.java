52d8ab1f9069cc8c7c7b3d186ac863082ec02196
CR <931009686@qq.com>
2020-10-20 18:26:27 +0800

    OpenFeign服务调用

---
+++ cloud-consumer-feign-order80/src/main/java/com/cr/springcloud/controller/OrderFeignController.java
@@ -0,0 +1,30 @@
+package com.cr.springcloud.controller;
+
+import com.cr.springcloud.entities.CommonResult;
+import com.cr.springcloud.entities.Payment;
+import com.cr.springcloud.service.PaymentFeignService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * @author cr
+ * @date 2020-10-20 18:10
+ */
+@Slf4j
+@RestController
+@RequestMapping("/consumer/payments")
+public class OrderFeignController {
+    @Autowired
+    private PaymentFeignService paymentFeignService;
+
+    @PostMapping()
+    public CommonResult<Integer> create(@RequestBody Payment payment) {
+        return paymentFeignService.create(payment);
+    }
+
+    @GetMapping("/{id}")
+    public CommonResult<Payment> getPaymentById(@PathVariable Long id) {
+        return paymentFeignService.getPaymentById(id);
+    }
+}
---
+++ cloud-consumer-feign-order80/src/main/java/com/cr/springcloud/service/PaymentFeignService.java
@@ -0,0 +1,22 @@
+package com.cr.springcloud.service;
+
+import com.cr.springcloud.entities.CommonResult;
+import com.cr.springcloud.entities.Payment;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+/**
+ * @author cr
+ * @date 2020-10-20 18:05
+ */
+@FeignClient(value = "CLOUD-PAYMENT-SERVICE", path = "/payments")
+public interface PaymentFeignService {
+    @PostMapping()
+    CommonResult<Integer> create(@RequestBody Payment payment);
+
+    @GetMapping("/{id}")
+    CommonResult<Payment> getPaymentById(@PathVariable("id") Long id);
+}
