963a30b222236bc9837e510fa23fe02a82234c26
CR <931009686@qq.com>
2020-10-21 16:23:32 +0800

    Hystrix服务降级订单侧fallback

--- cloud-consumer-feign-hystrix-order80/src/main/java/con/cr/springcloud/controller/OrderHystrixController.java
+++ cloud-consumer-feign-hystrix-order80/src/main/java/con/cr/springcloud/controller/OrderHystrixController.java
@@ -1,5 +1,7 @@
 package con.cr.springcloud.controller;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import con.cr.springcloud.service.PaymentHystrixService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,7 +27,13 @@ public class OrderHystrixController {
     }
 
     @GetMapping("/hystrix/timeout/{id}")
+    @HystrixCommand(fallbackMethod = "paymentTimeOutFallbackMethod", commandProperties = {@HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "1500")})
     public String paymentInfoTimeOut(@PathVariable Integer id) {
+        int age = 10 / 0;
         return paymentHystrixService.paymentInfoTimeOut(id);
+    }
+
+    public String paymentTimeOutFallbackMethod(@PathVariable("id") Integer id) {
+        return "我是消费者80，对方支付系统繁忙请10秒钟后再试或者自己运行出错请检查自己";
     }
 }
