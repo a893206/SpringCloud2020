47ae6d21c433a072805e168fdd87679188da9cbb
CR <931009686@qq.com>
2020-10-21 20:40:27 +0800

    Hystrix全局服务降级DefaultProperties

--- cloud-consumer-feign-hystrix-order80/src/main/java/con/cr/springcloud/controller/OrderHystrixController.java
+++ cloud-consumer-feign-hystrix-order80/src/main/java/con/cr/springcloud/controller/OrderHystrixController.java
@@ -1,7 +1,7 @@
 package con.cr.springcloud.controller;
 
+import com.netflix.hystrix.contrib.javanica.annotation.DefaultProperties;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
-import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import con.cr.springcloud.service.PaymentHystrixService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 @RestController
 @RequestMapping("/consumer/payments")
+@DefaultProperties(defaultFallback = "paymentGlobalFallbackMethod")
 public class OrderHystrixController {
     @Autowired
     private PaymentHystrixService paymentHystrixService;
@@ -27,7 +28,8 @@ public class OrderHystrixController {
     }
 
     @GetMapping("/hystrix/timeout/{id}")
-    @HystrixCommand(fallbackMethod = "paymentTimeOutFallbackMethod", commandProperties = {@HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "1500")})
+    //@HystrixCommand(fallbackMethod = "paymentTimeOutFallbackMethod", commandProperties = {@HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "1500")})
+    @HystrixCommand
     public String paymentInfoTimeOut(@PathVariable Integer id) {
         int age = 10 / 0;
         return paymentHystrixService.paymentInfoTimeOut(id);
@@ -35,5 +37,9 @@ public class OrderHystrixController {
 
     public String paymentTimeOutFallbackMethod(@PathVariable("id") Integer id) {
         return "我是消费者80，对方支付系统繁忙请10秒钟后再试或者自己运行出错请检查自己";
+    }
+
+    public String paymentGlobalFallbackMethod() {
+        return "Global异常处理信息，请稍后再试";
     }
 }
