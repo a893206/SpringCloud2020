c8d9add37899b1ddc2a7cd82c4e40c426103ea14
CR <931009686@qq.com>
2020-10-22 17:53:40 +0800

    Gateway9527搭建

---
+++ cloud-gateway-gateway9527/src/main/java/com/cr/springcloud/GatewayMain9527.java
@@ -0,0 +1,17 @@
+package com.cr.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author cr
+ * @date 2020-10-22 17:40
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class GatewayMain9527 {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayMain9527.class, args);
+    }
+}
