396ea7ca06d1ae0ffc44f65d1d4b7525204cbd39
CR <931009686@qq.com>
2020-10-26 00:37:52 +0800

    Stream消息驱动消费者

---
+++ cloud-stream-rabbitmq-consumer8802/src/main/java/com/cr/springcloud/StreamMQMain8802.java
@@ -0,0 +1,15 @@
+package com.cr.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author cr
+ * @date 2020-10-26 00:23
+ */
+@SpringBootApplication
+public class StreamMQMain8802 {
+    public static void main(String[] args) {
+        SpringApplication.run(StreamMQMain8802.class, args);
+    }
+}
