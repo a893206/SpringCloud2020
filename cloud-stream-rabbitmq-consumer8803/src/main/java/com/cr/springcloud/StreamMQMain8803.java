732343a39b58b2b938ada50d9e3264ea4abc5d1d
CR <931009686@qq.com>
2020-10-26 23:06:42 +0800

    Stream group解决消息重复消费

---
+++ cloud-stream-rabbitmq-consumer8803/src/main/java/com/cr/springcloud/StreamMQMain8803.java
@@ -0,0 +1,15 @@
+package com.cr.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author cr
+ * @date 2020-10-26 22:44
+ */
+@SpringBootApplication
+public class StreamMQMain8803 {
+    public static void main(String[] args) {
+        SpringApplication.run(StreamMQMain8803.class, args);
+    }
+}
