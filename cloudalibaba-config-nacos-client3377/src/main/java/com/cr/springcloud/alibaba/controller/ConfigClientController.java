6d5a400d267e3f9e65bfb9ed1c6d71bb0664688a
CR <931009686@qq.com>
2020-10-28 23:42:25 +0800

    Nacos服务配置中心

---
+++ cloudalibaba-config-nacos-client3377/src/main/java/com/cr/springcloud/alibaba/controller/ConfigClientController.java
@@ -0,0 +1,22 @@
+package com.cr.springcloud.alibaba.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author cr
+ * @date 2020-10-28 23:24
+ */
+@RestController
+@RefreshScope
+public class ConfigClientController {
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("/config/info")
+    public String getConfigInfo() {
+        return configInfo;
+    }
+}
